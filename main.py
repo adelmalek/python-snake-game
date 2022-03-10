1014b81d34c987ed6af2b4c50f76b317d442aa8b
Adel <gerencser.adel@gmail.com>
2022-03-10 11:07:07 +0100

    create gameboard

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ GAME_HEIGHT = 700
 SPEED = 100
 SPACE_SIZE = 50
 BODY_PARTS = 3
-SNAKE_COLOR = "#00FF00"
-FOOD_COLOR = "#FF0000"
-BACKGROUND_COLOR = "#000000"
+SNAKE_COLOR = "#FFFFFF"
+FOOD_COLOR = "#FFFF00"
+BACKGROUND_COLOR = "#0000FF"
 
 class Snake:
     pass
@@ -31,5 +31,26 @@ def game_over():
 window = Tk()
 window.title("Snake Game")
 window.resizable(False, False)
+
+score = 0
+direction = "down"
+
+label = Label(text="Score: {}".format(score), font=("consolas", 20))
+label.pack()
+
+canvas = Canvas(window, width=GAME_WIDTH, height=GAME_HEIGHT, bg=BACKGROUND_COLOR)
+canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 window.mainloop()
